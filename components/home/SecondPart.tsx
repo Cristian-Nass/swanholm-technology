7d6b695c361f6cd9a20e5ac56b6ad05d7521b1ea
Cristian Nass <cristian.tirdad@gmail.com>
2025-09-10 13:16:07 +0200

    Translation

--- components/home/SecondPart.tsx
+++ components/home/SecondPart.tsx
@@ -1,17 +1,12 @@
+import {useTranslations} from 'next-intl';
+
 const SecondPart = () => {
+  const t = useTranslations('home');
   return (
     <div className="flex flex-col gap-[16px] bg-cyan-700 px-4 py-8 rounded-lg w-full text-white">
-      <h1 className="text-3xl font-bold">Why smart workwear?</h1>
-      <p>
-        Accidents can be identified much faster with the use of AI. Workplace
-        accidents due to poor visibility, slippery or unstable ground, working
-        alone or in other hazardous environments could be avoided with the use
-        of smart technology.
-      </p>
-      <p className="text-lg">
-        By integrating the technology into the workwear you make sure that it is
-        used.
-      </p>
+      <h1 className="text-3xl font-bold">{t('secondPartTitle')}</h1>
+      <p>{t('secondPartDescription')}</p>
+      <p className="text-lg">{t('secondPartDescription2')}</p>
     </div>
   );
 };
