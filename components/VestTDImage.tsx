8078e3b61859438d9f649723c268f223d7cd9c59
Cristian Nass <cristian.tirdad@gmail.com>
2025-09-10 14:54:50 +0200

    save

--- components/VestTDImage.tsx
+++ components/VestTDImage.tsx
@@ -1,12 +1,17 @@
+'use client';
 import Image from 'next/image';
+import {useMediaQuery} from '@/hooks/use-media-query';
+
 const VestTDImage = () => {
+  const isDesktop = useMediaQuery('(min-width: 1024px)');
+  console.log(isDesktop);
   return (
     <div>
       <Image
         src="/assets/images/3d-vest.png"
         alt="VestTD"
-        width={300}
-        height={300}
+        width={isDesktop ? 400 : 200}
+        height={isDesktop ? 400 : 200}
       />
     </div>
   );
---
+++ components/home/FirstPart.tsx
@@ -0,0 +1,20 @@
+import VestTDImage from '../VestTDImage';
+import {useTranslations} from 'next-intl';
+const FirstPart = () => {
+  const t = useTranslations('home');
+  return (
+    <div className="flex flex-col lg:flex-row gap-[16px] h-full items-center">
+      <div>
+        <VestTDImage />
+      </div>
+      <div className="flex flex-col gap-[16px]">
+        <h1 className="text-4xl font-bold">{t('title')}</h1>
+        <h3 className="text-2xl font-bold">{t('subtitle')}</h3>
+        <p>{t('description')}</p>
+        <p>{t('description2')}</p>
+      </div>
+    </div>
+  );
+};
+
+export default FirstPart;
