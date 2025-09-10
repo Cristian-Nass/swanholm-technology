d6a8452569665c0909ccc47312a3c590e445a269
Cristian Nass <cristian.tirdad@gmail.com>
2025-09-10 12:24:14 +0200

    Add 3d Image

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,15 +1,23 @@
 import Image from 'next/image';
 import {useTranslations} from 'next-intl';
+import VestTDImage from '@/components/VestTDImage';
 export default function Home() {
   const t = useTranslations('home');
   return (
     <>
       <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
         <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-          <h1 className="text-4xl font-bold">{t('title')}</h1>
-          <h3 className="text-2xl font-bold">{t('subtitle')}</h3>
-          <p>{t('description')}</p>
-          <p>{t('description2')}</p>
+          <div className="flex gap-[16px] h-full items-center">
+            <div>
+              <VestTDImage />
+            </div>
+            <div className="flex flex-col gap-[16px]">
+              <h1 className="text-4xl font-bold">{t('title')}</h1>
+              <h3 className="text-2xl font-bold">{t('subtitle')}</h3>
+              <p>{t('description')}</p>
+              <p>{t('description2')}</p>
+            </div>
+          </div>
         </main>
         <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
           <a
---
+++ components/VestTDImage.tsx
@@ -0,0 +1,15 @@
+import Image from 'next/image';
+const VestTDImage = () => {
+  return (
+    <div>
+      <Image
+        src="/assets/images/3d-vest.png"
+        alt="VestTD"
+        width={300}
+        height={300}
+      />
+    </div>
+  );
+};
+
+export default VestTDImage;
