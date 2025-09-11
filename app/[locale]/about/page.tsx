414639ac211a1cc01285d9cd8fa0e5504d164919
Cristian Nass <cristian.tirdad@gmail.com>
2025-09-11 23:35:45 +0200

    Save

--- app/[locale]/about/page.tsx
+++ app/[locale]/about/page.tsx
@@ -1,8 +1,8 @@
 import Image from "next/image";
 const AboutPage = () => {
   return (
-    <div className="flex gap-[16px] justify-center max-w-[1200px] mx-auto bg-cyan-100 my-[32px] p-[26px] rounded-[16px]">
-      <div className="w-[300px]">
+    <div className="flex lg:flex-row flex-col gap-[16px] justify-center max-w-[1200px] mx-auto my-[32px] p-[26px] rounded-[16px]">
+      <div className="w-[200px] md:w-[300px] lg:w-[500px] flex items-center justify-center mx-auto">
         <Image
           src="/assets/images/vest-d.webp"
           alt="About"
@@ -10,24 +10,24 @@ const AboutPage = () => {
           height={430}
         />
       </div>
-      <article className="flex flex-col gap-[16px] p-[16px]">
-        <p className="text-3xl font-bold text-cyan-900">
+      <article className="flex flex-col lg:gap-[16px] gap-[8px] px-[16px] lg:pt-14 pt-0">
+        <p className="text-3xl md:text-2xl lg:text-5xl font-bold text-cyan-900">
           How can we make the profession as a truck driver safer?
         </p>
-        <p className="text-lg">
+        <p className="text-base md:text-lg lg:text-xl">
           That question was the ignition for the connected safety vest, that
           initially grew into a project at a major Swedish truck manufacturer.{" "}
         </p>
-        <p className="text-lg">
+        <p className="text-base md:text-lg lg:text-xl">
           Swanholm Technology was founded in 2020 when the project was taken
           over.
         </p>
-        <p className="text-lg">
+        <p className="text-base md:text-lg lg:text-xl">
           Our mission is to improve people&apos;s safety by offering smart,
           connected safety products for exposed professions as well as for
           people with an active lifestyle.
         </p>
-        <p className="text-lg">
+        <p className="text-base md:text-lg lg:text-xl">
           The Alert Vest is the first line of products from Swanholm Technology
           launched in 2022.
         </p>
