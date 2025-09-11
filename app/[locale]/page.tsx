0bdc1cad9967e552749d2a549ee4a05f14593b1d
Cristian Nass <cristian.tirdad@gmail.com>
2025-09-12 00:11:24 +0200

    change mani to section from page

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -5,14 +5,14 @@ export default function Home() {
     <>
       {/* <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20"> */}
       <div className="font-sans grid justify-items-center px-4 sm:px-8">
-        <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <section className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
           <FirstPart />
           <SecondPart />
           <SecondPart />
           <SecondPart />
           <SecondPart />
           <SecondPart />
-        </main>
+        </section>
       </div>
     </>
   );
