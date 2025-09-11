48ac97875cba0488e98cf67a940c4967dcaadd9c
Cristian Nass <cristian.tirdad@gmail.com>
2025-09-12 00:19:01 +0200

    save

--- app/[locale]/about/page.tsx
+++ app/[locale]/about/page.tsx
@@ -2,14 +2,14 @@ import Image from "next/image";
 const AboutPage = () => {
   return (
     <section className="flex lg:flex-row flex-col gap-[16px] justify-center max-w-[1200px] mx-auto lg:my-[32px] p-[26px] lg:rounded-[16px] bg-linear-to-r/srgb from-cyan-200 to-slate-100">
-      <div className="w-[200px] md:w-[300px] lg:w-[500px] flex items-center justify-center mx-auto">
+      <article className="w-[200px] md:w-[300px] lg:w-[500px] flex items-center justify-center mx-auto">
         <Image
           src="/assets/images/vest-d.webp"
           alt="About"
           width={300}
           height={430}
         />
-      </div>
+      </article>
       <article className="flex flex-col lg:gap-[16px] gap-[8px] px-[16px] lg:pt-14 pt-0">
         <p className="text-3xl md:text-2xl lg:text-5xl font-bold text-cyan-900">
           How can we make the profession as a truck driver safer?
--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -2,18 +2,15 @@ import SecondPart from "@/components/home/SecondPart";
 import FirstPart from "@/components/home/FirstPart";
 export default function Home() {
   return (
-    <>
-      {/* <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20"> */}
-      <div className="font-sans grid justify-items-center px-4 sm:px-8">
-        <section className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-          <FirstPart />
-          <SecondPart />
-          <SecondPart />
-          <SecondPart />
-          <SecondPart />
-          <SecondPart />
-        </section>
-      </div>
-    </>
+    <section className="font-sans grid justify-items-center px-4 sm:px-8">
+      <article className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <FirstPart />
+        <SecondPart />
+        <SecondPart />
+        <SecondPart />
+        <SecondPart />
+        <SecondPart />
+      </article>
+    </section>
   );
 }
