d44debec1c5678360e9a578b82faf020beca0148
Cristian Nass <cristian.tirdad@gmail.com>
2025-09-30 11:17:59 +0200

    save

--- components/home/NewStyle.tsx
+++ components/home/NewStyle.tsx
@@ -2,18 +2,21 @@ import Image from "next/image";
 const NewStyle = () => {
   return (
     <section className="w-full background-gradient">
-      <div className="flex max-w-[1280px] w-full h-[400px] mx-auto flex-nowrap background-gradient">
+      <div className="flex max-w-[1280px] w-full h-[500px] mx-auto flex-nowrap">
         <div className="flex-grow text-left p-[10px]">
-          <p>Text</p>
+          <p className="text-3xl font-bold text-yellow-200 italic mt-4 font-serif">
+            Be Connected. Be Safe.
+          </p>
         </div>
         <div
-          className="w-[266px] flex-shrink-0 bg-cover bg-center"
-          style={{backgroundImage: "url('/assets/images/01.webp')"}}>
+          className="w-[332px] flex-shrink-0 bg-cover bg-center"
+          // style={{backgroundImage: "url('/assets/images/01.webp')"}}
+        >
           <Image
-            src="/assets/images/01.webp"
+            src="/assets/images/03.webp"
             alt="New Style"
-            width={266}
-            height={266}
+            width={332}
+            height={332}
           />
         </div>
       </div>
