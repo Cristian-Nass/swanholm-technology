0f99c1eae5f681cf328a4c3b7ffd672cc5ba2316
Cristian Nass <cristian.tirdad@gmail.com>
2025-09-30 21:40:12 +0200

    save

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,13 +1,13 @@
-// import SecondPart from "@/components/home/SecondPart";
+import SecondPart from "@/components/home/SecondPart";
 // import FirstPart from "@/components/home/FirstPart";
 import NewStyle from "@/components/home/NewStyle";
 export default function Home() {
   return (
     <section>
-      <article className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+      <article className="flex flex-col gap-[2px] row-start-2 items-center sm:items-start">
         {/* <FirstPart /> */}
         <NewStyle />
-        {/* <SecondPart /> */}
+        <SecondPart />
       </article>
     </section>
   );
